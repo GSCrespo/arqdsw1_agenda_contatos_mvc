dfc30376b804d593a163defbf279545abac171ab
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-11-08 10:45:40 -0300

    Criação do controller para os novos contatos.

---
+++ src/main/java/br/edu/ifsp/dsw1/controller/IndexServlet.java
@@ -0,0 +1,24 @@
+package br.edu.ifsp.dsw1.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/index.do")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		var dispatcher = request.getRequestDispatcher("contact_form.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/main/webapp/includes/navBar.jsp
+++ src/main/webapp/includes/navBar.jsp
@@ -14,7 +14,7 @@
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item"><a class="nav-link active"
 					aria-current="page" href="#">Home</a></li>
-				<li class="nav-item"><a class="nav-link" href="#">Novo
+				<li class="nav-item"><a class="nav-link" href="index.do">Novo
 						Contato</a></li>
 				<li class="nav-item"><a class="nav-link" href="#">Contatos</a></li>
 			</ul>
