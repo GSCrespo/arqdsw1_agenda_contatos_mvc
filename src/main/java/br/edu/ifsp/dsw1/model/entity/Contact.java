550812eea2a9284a92f4e47be9aafe33eec6ab23
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-11-22 09:15:44 -0300

    Adicionando o lombok ao projeto.

--- src/main/java/br/edu/ifsp/dsw1/model/entity/Contact.java
+++ src/main/java/br/edu/ifsp/dsw1/model/entity/Contact.java
@@ -1,38 +1,15 @@
 package br.edu.ifsp.dsw1.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Contact {
 	private String name;
 	private String fone;
 	private String email;
-	
-	public Contact(String name, String fone, String email) {
-		this.name = name;
-		this.fone = fone;
-		this.email = email;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getFone() {
-		return fone;
-	}
-
-	public void setFone(String fone) {
-		this.fone = fone;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 	
 }
