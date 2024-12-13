3769dc7c844eb09f1cebc3d79c222880172436cc
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-12-13 09:22:38 -0300

    Ajustando Command para refletir alteração na busca.

--- src/main/java/br/edu/ifsp/dsw1/controller/command/SearchContactCommand.java
+++ src/main/java/br/edu/ifsp/dsw1/controller/command/SearchContactCommand.java
@@ -15,20 +15,18 @@ public class SearchContactCommand implements Command {
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		var email = request.getParameter("textEmail");
+		var name = request.getParameter("textName");
 		var dao = new ContactDaoFactory().factory();
 		
-		List<Contact> list;
-		var contact = dao.retrieve(email);
-		if (contact != null) { 
-			// Contato encontrado, apresenta a lista com apenas um contato.
-			list = List.of(contact);
-		} else {
-			// Contato não encontrado, apresenta-se a lista completa e mensagem de erro.
-			list = dao.retrieve();
-			request.setAttribute("errorMessage", "E-mail " + email + " não localizado.");
+		var contacts = dao.findByName(name);
+		
+		if (contacts.isEmpty()) {
+			// Contatos não encontrado, apresenta-se a lista completa e mensagem de erro.
+			contacts = dao.retrieve();
+			request.setAttribute("errorMessage", "Contato '" + name + "' não localizado.");
 		}
-		request.setAttribute("contacts", list);
+		
+		request.setAttribute("contacts", contacts);
 		
 		return "contacts.jsp";
 	}
